c603dca80873321c27c4c640fe1cb6cf735244bf
orhanmichi <orhanefe2424@gmail.com>
2025-12-25 21:34:46 +0300

    controllerlar olusturuldu ve food migration eklendi

---
+++ laravel/app/Models/Exercise.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Activity;
+
+class Exercise extends Model
+{
+    public function activities()
+    {
+        return $this->hasMany(Activity::class);
+    }
+}
