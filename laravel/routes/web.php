40a392c182eee47d87e03fb12d6cc88a518d325b
Test 2 <test2@istbtc.local>
2025-12-26 16:05:50 +0300

    web.php güncellendi

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,25 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PagesController;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\MealController;
+use App\Http\Controllers\ExerciseController;
+use App\Http\Controllers\ActivityController;
+use App\Http\Controllers\FoodController;
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get("/hello", function () {
-    return "Hello World";
-});
-Route::get("/hello/{name}", function ($name) {
-    return "Hello " . $name;
-});
-Route::get("/viewlike", function () {
-    return view("viewlike");
-});
-Route::get("/viewwithdata", function () {
-    $name = "Orhan Efe";
-    return view("viewwithdata", ["name" => $name]);
-});
-Route::get("/name", [namecontroller::class, "index"]);
-Route::get("/name/{name}", [namecontroller::class, "show"]);
-Route::get("/spanish", function () {
-    return view("spanishexceptions");
-});
+
+Route::get("/contact", [PagesController::class, "contact"]);
+
+Route::resource('customers', CustomerController::class);
+Route::resource('meals', MealController::class);
+Route::resource('exercises', ExerciseController::class);
+Route::resource('activities', ActivityController::class);
+Route::resource('foods', FoodController::class);
