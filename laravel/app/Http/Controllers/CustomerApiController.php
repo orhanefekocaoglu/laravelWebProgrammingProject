d14e0e1d9eeab9e31e3d8597386000ed577ebb10
orhanmichi <orhanefe2424@gmail.com>
2026-01-01 13:58:01 +0300

    api controller olusturdum ve get post sorguları attım.

---
+++ laravel/app/Http/Controllers/CustomerApiController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Customer;
+use Illuminate\Http\Request;
+
+class CustomerApiController extends Controller
+{
+    /**
+     * Return list of customers as JSON
+     */
+    public function index()
+    {
+        $customers = Customer::all();
+        return response()->json($customers);
+    }
+
+    /**
+     * Store a new customer and return JSON response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required|string|max:255',
+            'surname' => 'required|string|max:255',
+            'birthYear' => 'required|integer|min:1900|max:'.(date('Y')),
+            'gender' => 'required|string|max:10',
+        ]);
+
+        $customer = Customer::create($data);
+
+        return response()->json($customer, 201);
+    }
+}
